57da3c64a57d7765dda0c4a3224e135f6856faea
rahulbattula <96605083+RahulBattula@users.noreply.github.com>
2024-01-22 21:51:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,19 +3,24 @@ import librosa
 import numpy as np
 from tensorflow.keras.models import load_model
 from sklearn.preprocessing import LabelEncoder
+import plotly.express as px
 from io import BytesIO
 import plotly.graph_objects as go
 
 # Load the trained models and label encoders
 model_lstm = load_model("audio_classification_lstm.hdf5")
 model_sequential = load_model("audio_classification_sequential.hdf5")
-model_gru = load_model("audio_classification_gru.hdf5")
+# model_vggish = load_model("audio_classification_vggish.hdf5")
+model_gru = load_model("audio_classification_gru.hdf5") 
 
 labelencoder_lstm = LabelEncoder()
 labelencoder_lstm.classes_ = np.load("lstm_labelencoder.npy")
 
 labelencoder_sequential = LabelEncoder()
-labelencoder_sequential.classes_ = np.load("sequential_labelencoder.npy")
+labelencoder_sequential.classes_ = np.load("sequential_labelencoder.npy") 
+
+# labelencoder_vggish = LabelEncoder()
+# labelencoder_vggish.classes_ = np.load("vggish_labelencoder.npy")
 
 labelencoder_gru = LabelEncoder()
 labelencoder_gru.classes_ = np.load("gru_labelencoder.npy")
@@ -26,23 +31,19 @@ st.set_page_config(
     page_icon="🐦"  # You can use an emoji or provide a URL to an image
 )
 
-def predict_bird(audio_bytes, model, labelencoder, n_mfcc=21):
+def predict_bird(audio_bytes, model, labelencoder):
     # Load and preprocess the audio file for prediction
     audio, sample_rate = librosa.load(BytesIO(audio_bytes), res_type='kaiser_fast')
-    mfccs_features = librosa.feature.mfcc(y=audio, sr=sample_rate, n_mfcc=n_mfcc)
+    mfccs_features = librosa.feature.mfcc(y=audio, sr=sample_rate, n_mfcc=21)
     mfccs_scaled_features = np.mean(mfccs_features.T, axis=0)
     mfccs_scaled_features = mfccs_scaled_features.reshape(1, -1)
 
-    # For GRU, add an extra dimension for time steps
-    mfccs_scaled_features_gru = mfccs_scaled_features.reshape(1, mfccs_scaled_features.shape[1], 1)
-
     # Make prediction using the loaded model
-    predicted_label = model.predict(mfccs_scaled_features_gru)
+    predicted_label = model.predict(mfccs_scaled_features)
     predicted_class_index = np.argmax(predicted_label)
     prediction_class = labelencoder.inverse_transform([predicted_class_index])
 
     return prediction_class[0]
-
 
 # Set the title and description
 st.title("Bird Audio Classifier")
@@ -74,15 +75,19 @@ for i, uploaded_file in enumerate(uploaded_files):
         # Display loading message while predicting
         with st.spinner(f"Predicting for {uploaded_file.name}..."):
             # Predict using LSTM model
-            predicted_bird_lstm = predict_bird(audio_bytes, model_lstm, labelencoder_lstm, n_mfcc=40)
+            predicted_bird_lstm = predict_bird(audio_bytes, model_lstm, labelencoder_lstm)
             st.success(f"For {uploaded_file.name}, LSTM predicted bird is: {predicted_bird_lstm}")
 
             # Predict using Sequential model
-            predicted_bird_sequential = predict_bird(audio_bytes, model_sequential, labelencoder_sequential, n_mfcc=40)
+            predicted_bird_sequential = predict_bird(audio_bytes, model_sequential, labelencoder_sequential)
             st.success(f"For {uploaded_file.name}, Sequential predicted bird is: {predicted_bird_sequential}")
 
+            # Predict using VGGish model
+            # predicted_bird_vggish = predict_bird(audio_bytes, model_vggish, labelencoder_vggish)
+            # st.success(f"For {uploaded_file.name}, VGGish predicted bird is: {predicted_bird_vggish}")
+
             # Predict using GRU model
-            predicted_bird_gru = predict_bird(audio_bytes, model_gru, labelencoder_gru, n_mfcc=40)
+            predicted_bird_gru = predict_bird(audio_bytes, model_gru, labelencoder_gru)
             st.success(f"For {uploaded_file.name}, GRU predicted bird is: {predicted_bird_gru}")
 
 # Sidebar with app description and contact information
